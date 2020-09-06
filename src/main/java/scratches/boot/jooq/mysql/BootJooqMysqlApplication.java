b89d4bd6f896acf2603075280f5e69f85b40fc2c
Rashidi Zin <rashidi@zin.my>
2020-09-06 10:57:09 +0800

    Initial commit
    Generated by Spring Initializr.

---
+++ src/main/java/scratches/boot/jooq/mysql/BootJooqMysqlApplication.java
@@ -0,0 +1,13 @@
+package scratches.boot.jooq.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJooqMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootJooqMysqlApplication.class, args);
+    }
+
+}
